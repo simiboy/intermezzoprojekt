425228b67ec56d043beea4ed26eca3683fdecba3
simiboy <65280920+simiboy@users.noreply.github.com>
2025-02-17 16:29:13 +0100

    starting off

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const arrow = document.getElementById('arrow');
+        if (arrow) {
+            arrow.style.display = 'inline-block';
+        }
+    }, 3000);
+});
